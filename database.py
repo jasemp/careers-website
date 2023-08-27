77427a51a66ed33800a43f127618ec21e70decae
jasemp <142363672+jasemp@users.noreply.github.com>
2023-08-27 08:18:13 +0000

    job pages

--- database.py
+++ database.py
@@ -16,3 +16,13 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM jobs WHERE id = {id}"))
+    row = result.all()
+    if len(row) == 0:
+      return None
+    else:
+      return row[0]._asdict()
